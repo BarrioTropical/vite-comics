0c25a311c8c5ebaa049794028110623f78cdf734
BarrioTropical <barriotropical@gmail.com>
2022-11-10 14:58:58 +0100

    sass e create components
    Signed-off-by: BarrioTropical <“barriotropical@gmail.com”>

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
+
+<template>
+  <HeaderComponent/>
+
+  <main>
+    <MainComponent/>
+  </main>
+
+  <FooterComponent/>
+</template>
+
 <script setup>
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
 
 </script>
 
-<template>
-  
-</template>
 
 <style scoped>
 
